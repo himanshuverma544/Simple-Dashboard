91de8ea58a9a1a1c8edd2d6ee7fde1b3362f8ffe
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-10 23:57:17 +0530

    Revert "adding up the env variables code"
    This reverts commit 92ce1437c8b05145823f23e63eda77df549fca45.

--- php/database.php
+++ php/database.php
@@ -1,10 +1,26 @@
 <?php
 
 // Database
-$serverName = getenv("MYSQLHOST");
-$userName = getenv("MYSQLUSER");
-$password = getenv("MYSQLPASSWORD");
-$databaseName = getenv("MYSQLDATABASE");
+
+$fileName = !isset($_GET["id"]) ? "./credentials.txt" : "../credentials.txt";
+
+try {
+	$credFile = fopen($fileName, "r") or die("Unable to open the file.");
+	$credFileData = explode(' ', fread($credFile, filesize($fileName)));	
+} 
+catch(Exception $exception) {
+	echo 'Message: '.$exception->getMessage();
+}
+finally {
+	fclose($credFile);
+}
+
+
+
+$serverName = $credFileData[0];
+$userName = $credFileData[1];
+$password = $credFileData[2];
+$databaseName = $credFileData[3];
 
 // Create Connection
 $conn = new mysqli($serverName,$userName,$password,$databaseName);
