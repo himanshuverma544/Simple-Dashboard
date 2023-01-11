323d47bbdb347f29ad2cf32add6d57b2e89ecb29
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-12 03:25:54 +0530

    rolling back changes to ideal condition

--- php/database.php
+++ php/database.php
@@ -2,7 +2,6 @@
 require_once('dependencies/parse-env.php');
 use DevCoder\DotEnv;
 
-// Keeping it in "try-catch block" for making it run on the local, staging and production.
 try {
 		$dotenv = new DotEnv();
 		$dotenv->load();
@@ -14,24 +13,7 @@ $userName = $_ENV["MYSQLUSER"];
 $password = $_ENV["MYSQLPASSWORD"];
 $databaseName = $_ENV["MYSQLDATABASE"];
 
-
-// echo "$serverName - $userName - $password - $databaseName";
-
-
-// Create Connection
-
-try {
-	$conn = new mysqli($serverName, $userName, $password, $databaseName);
-}
-catch(Exception $e) {
-	echo 'Message: '.$e->getMessage();
-	die('died in catch');
-}
-finally{
-	die('In finally');
-}
-
-
+$conn = new mysqli($serverName, $userName, $password, $databaseName);
 if($conn->connect_error)
 die("Connection Failed!<br>".$conn->connect_error);
 ?>
