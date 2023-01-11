f1a6bdb508f82286e009b180e788fa6ce238ce81
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-12 01:18:46 +0530

    added class to fetch env variables

--- php/database.php
+++ php/database.php
@@ -1,22 +1,22 @@
 <?php
 
 // Database
-declare(strict_types=1);
-require_once('vendor/autoload.php');
-$project_dir_path = str_replace("\php", "", __DIR__);
+
+require_once('dependencies/parse-env.php');
+use DevCoder\DotEnv;
 
 try {
-		$dotenv = Dotenv\Dotenv::createImmutable($project_dir_path);
+		$dotenv = new DotEnv();
 		$dotenv->load();
 }
 catch(Exception $exception) {
 		echo 'Message: '.$exception->getMessage();
 }
 
-$serverName = $_ENV["DB_SERVER_NAME"];
-$userName = $_ENV["DB_USERNAME"];
-$password = $_ENV["DB_PASSWORD"];
-$databaseName = $_ENV["DB_NAME"];
+$serverName = $_ENV["MYSQLHOST"];
+$userName = $_ENV["MYSQLUSER"];
+$password = $_ENV["MYSQLPASSWORD"];
+$databaseName = $_ENV["MYSQLDATABASE"];
 
 // Create Connection
 $conn = new mysqli($serverName,$userName,$password,$databaseName);
