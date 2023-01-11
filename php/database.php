cb659ea7418254ca4ac51c3fc1912e84b9b25c56
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-12 02:21:26 +0530

    debugging phase - 9

--- php/database.php
+++ php/database.php
@@ -3,24 +3,24 @@
 // Database
 
 require_once('dependencies/parse-env.php');
-use DevCoder\DotEnv;
+use DevCoder\DotEnv as E;
 
 try {
-		// $dotenv = new DotEnv();
-		// $dotenv->load();
+		$dotenv = new E();
+		$dotenv->load();
 }
 catch(Exception $exception) {
 		echo 'Message: '.$exception->getMessage();
 }
 
 $serverName = $_ENV["MYSQLHOST"];
-$userName = $_ENV["MYSQLUSER"];
-$password = $_ENV["MYSQLPASSWORD"];
-$databaseName = $_ENV["MYSQLDATABASE"];
+// $userName = $_ENV["MYSQLUSER"];
+// $password = $_ENV["MYSQLPASSWORD"];
+// $databaseName = $_ENV["MYSQLDATABASE"];
 
 
-die($serverName);
-
+ die($serverName);
+// die;
 // Create Connection
 $conn = new mysqli($serverName,$userName,$password,$databaseName);
 
