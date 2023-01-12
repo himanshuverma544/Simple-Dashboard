5ae9afa3fc8778db856f8cc980f2ece202d9708e
Himanshu Verma <himanshuverma544@gmail.com>
2023-01-13 00:45:53 +0530

    adding port

--- php/database.php
+++ php/database.php
@@ -12,8 +12,9 @@ $serverName = $_ENV["MYSQLHOST"];
 $userName = $_ENV["MYSQLUSER"];
 $password = $_ENV["MYSQLPASSWORD"];
 $databaseName = $_ENV["MYSQLDATABASE"];
+$port = $_ENV["MYSQLPORT"];
 
-$conn = new mysqli($serverName, $userName, $password, $databaseName);
+$conn = new mysqli($serverName, $userName, $password, $databaseName, $port);
 if($conn->connect_error)
 die("Connection Failed!<br>".$conn->connect_error);
 ?>
